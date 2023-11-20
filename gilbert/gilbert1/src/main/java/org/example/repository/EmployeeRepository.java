71afafed4a31e024907635a8665d67687a955157
gilbertmphago <49983260+gilbertmphago@users.noreply.github.com>
2023-11-20 20:21:00 +0200

    Delete gilbert directory

--- gilbert/gilbert1/src/main/java/org/example/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.model.employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface EmployeeRepository extends JpaRepository<employee, Long> {
-// all crud database methods
-}
--- gilbert/springbootmapping/src/main/java/controller/controller/usercontroller.java
+++
@@ -1,4 +0,0 @@
-package controller.controller;
-
-public class usercontroller {
-}
